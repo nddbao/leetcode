a1d48b8c38aed346359c8ecd2fce1ea0bd51b434
nddbao <hikaruit12@gmail.com>
2021-10-11 17:30:16 +0700

    add solution for 239

---
+++ lesson-03/queue/239-sliding-window-maximum/solution.go
@@ -0,0 +1,31 @@
+package solution
+
+/*
+	leetcode: https://leetcode.com/problems/sliding-window-maximum/
+*/
+
+func maxSlidingWindow(nums []int, k int) []int {
+	var result, qVal, qIdx []int
+	for i := 0; i < len(nums); i++ {
+		for len(qVal) != 0 && qVal[len(qVal)-1] <= nums[i] {
+			qVal = qVal[:len(qVal)-1]
+			qIdx = qIdx[:len(qIdx)-1]
+		}
+
+		// add nums[i] to the end
+		qVal = append(qVal, nums[i])
+		qIdx = append(qIdx, i)
+
+		// remove nums[i-k]
+		if i >= k && qIdx[0] == i-k {
+			qVal = qVal[1:]
+			qIdx = qIdx[1:]
+		}
+
+		if i >= k-1 {
+			result = append(result, qVal[0])
+		}
+	}
+
+	return result
+}
