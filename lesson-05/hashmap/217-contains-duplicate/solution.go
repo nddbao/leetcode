af91ac5ada9c513dd95a46b2e3a24ba9cb99addc
nddbao <hikaruit12@gmail.com>
2021-10-20 00:03:05 +0700

    add explain for 217

--- lesson-05/hashmap/217-contains-duplicate/solution.go
+++ lesson-05/hashmap/217-contains-duplicate/solution.go
@@ -4,6 +4,17 @@ package solution
 	leetcode: https://leetcode.com/problems/contains-duplicate/
 */
 
+/*
+	First thinking, we can use brute force to check every element in array.
+	So time complexity will be O(n^2)
+
+	Let's try to use map. We loop through the array and put element in map.
+	If we see current element already exists in map, we know that is duplicate.
+
+	Time complexity: O(n)
+	Space complexity: O(n)
+
+*/
 func containsDuplicate(nums []int) bool {
 	distinct := make(map[int]struct{})
 	for _, v := range nums {
