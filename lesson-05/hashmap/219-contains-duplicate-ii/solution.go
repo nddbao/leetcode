165511ad4c4d4b66442debd23ca7e2ec669c3a25
nddbao <hikaruit12@gmail.com>
2021-10-20 00:17:34 +0700

    add explain for 219

--- lesson-05/hashmap/219-contains-duplicate-ii/solution.go
+++ lesson-05/hashmap/219-contains-duplicate-ii/solution.go
@@ -4,6 +4,17 @@ package solution
 	leetcode: https://leetcode.com/problems/contains-duplicate-ii/
 */
 
+/*
+	We can use brute force check combination of two values in the array.
+	But time complexity will be O(n^2)
+
+	So we can do it by one pass through the array by using map to store (value, index)
+	Every iteration, we check current value existing in map.
+	If existing, check two indexes satisfy  our requirement or not.
+
+	Time complexity: O(n)
+	Space complexity: O(n)
+*/
 func containsNearbyDuplicate(nums []int, k int) bool {
 	distinctElements := make(map[int]int)
 	for j := 0; j < len(nums); j++ {
