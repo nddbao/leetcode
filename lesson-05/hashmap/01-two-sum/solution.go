b4910e77ce9b0b61b879375732adae475f660595
nddbao <hikaruit12@gmail.com>
2021-10-19 23:55:02 +0700

    add solution for 01

--- lesson-05/hashmap/01-two-sum/solution.go
+++ lesson-05/hashmap/01-two-sum/solution.go
@@ -1,5 +1,23 @@
 package solution
 
+/*
+	leetcode: https://leetcode.com/problems/two-sum
+*/
+
+/*
+	Basically, we can use brute force to solve this problem.
+	By checking every combination of two values whether they can sum up to target.
+	But time complexity will be O(n^2).
+
+	We can do this better with one pass by using map.
+	Iterate through the array and map value to index.
+	we check (target- value) that is exist in map.
+	If exist, we know the result to return.
+
+	Time complexity: O(n)
+	Space complexity: O(n)
+*/
+
 func twoSum(nums []int, target int) []int {
 	distinct := make(map[int]int)
 	for i1, v := range nums {
