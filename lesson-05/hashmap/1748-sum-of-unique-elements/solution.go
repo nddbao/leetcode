fc3c3e8e9802d76c983e1232ce574ed56b1d806e
nddbao <hikaruit12@gmail.com>
2021-10-20 00:39:23 +0700

    add explain for 1748

--- lesson-05/hashmap/1748-sum-of-unique-elements/solution.go
+++ lesson-05/hashmap/1748-sum-of-unique-elements/solution.go
@@ -4,6 +4,16 @@ package solution
 	leetcode: https://leetcode.com/problems/sum-of-unique-elements/
 */
 
+/*
+	We know range of value array will be [1..100]
+	So we just go through, count value occurrence and put it in other array.
+	And after that, we can sum up for value have occurrence equal to 1.
+
+	Time complexity: O(n)
+	Space complexity: O(1)
+
+*/
+
 func sumOfUnique(nums []int) int {
 	var a [101]int
 	for _, v := range nums {
