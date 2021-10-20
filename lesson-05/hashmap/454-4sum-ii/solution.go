e14088d3967344d45aa6780561d96dde803de347
nddbao <hikaruit12@gmail.com>
2021-10-20 17:28:38 +0700

    add explain for 454

--- lesson-05/hashmap/454-4sum-ii/solution.go
+++ lesson-05/hashmap/454-4sum-ii/solution.go
@@ -1,5 +1,23 @@
 package solution
 
+/*
+	leetcode: https://leetcode.com/problems/4sum-ii/
+*/
+
+/*
+	Using an map to store frequent sum from all combination
+	that can sum up from nums1 and nums1.
+
+	Then we check sum of all combination that can sum up from num3 and nums4.
+	We have: s = nums3 + num4 = - (nums1 + nums2)
+	if  (-s) found in map, we know for sure: nums1 + nums2 + nums3 + nums4 = 0
+	Just count it.
+
+	Time complexity: O(n^2)
+	Space complexity: O(n^2)
+
+*/
+
 func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
 	mSum := make(map[int]int)
 	for _, v1 := range nums1 {
