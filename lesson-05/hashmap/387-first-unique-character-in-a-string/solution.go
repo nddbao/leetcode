6519d53b12105a8458ecc1f5a4d67053aa05d0ca
nddbao <hikaruit12@gmail.com>
2021-10-20 16:05:21 +0700

    add explain for 387

--- lesson-05/hashmap/387-first-unique-character-in-a-string/solution.go
+++ lesson-05/hashmap/387-first-unique-character-in-a-string/solution.go
@@ -4,6 +4,30 @@ package solution
 	leetcode: https://leetcode.com/problems/first-unique-character-in-a-string/
 */
 
+/*
+	We can use an array charArr that represents for 26 alphabet. Init value will be -1.
+	This array will store the index that character appears in string.
+	For example:
+		charArray [-1 -1 .. -1]
+		idx = character - 'a' -> value of 'c' will have idx 2
+
+	We go through string and check current character is set or not in charArray.
+	If not, we will set the its index. Otherwise, we set to -2.
+	For example:
+	 	S = "acabdd"
+					  'a' 'b' 'c' 'd' ....
+		=> charArray [ -2  3   1   -2	-1  ... -1]
+
+	We know that character appears 1 time in string if and only if value in charArray >= 0
+	Just checking charArray with that constraint and find MIN.
+	MIN is our result.
+
+	Time complexity: O(n)
+	Space complexity: O(1)
+
+
+*/
+
 func firstUniqChar(s string) int {
 	charArr := newCharArray()
 
