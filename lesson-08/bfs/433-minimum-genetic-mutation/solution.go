835fee11e315376aead8387b68e3ccc13616e5b4
nddbao <hikaruit12@gmail.com>
2021-10-31 15:00:44 +0700

    [lesson-08/bfs] add solution for 433

---
+++ lesson-08/bfs/433-minimum-genetic-mutation/solution.go
@@ -0,0 +1,112 @@
+package solution
+
+/*
+	leetcode: https://leetcode.com/problems/minimum-genetic-mutation/
+*/
+
+func minMutation(start string, end string, bank []string) int {
+	if start == end {
+		return 0
+	} else if len(bank) == 0 {
+		return -1
+	}
+
+	startIdx, _ := pushToBank(&bank, start)
+	endIdx, isPushed := pushToBank(&bank, end)
+	if isPushed {
+		return -1
+	}
+
+	isConnected := buildConnected(bank)
+	visited := make([]bool, len(isConnected))
+
+	return bfs(isConnected, visited, startIdx, endIdx)
+}
+
+func bfs(isConnected [][]int, visited []bool, startIdx, endIdx int) int {
+	var level int
+	visited[startIdx] = true
+
+	q := &Queue{}
+	q.Enque(startIdx)
+	for q.Size() > 0 {
+		size := q.Size()
+		level++
+		for k := 0; k < size; k++ {
+			node := q.Deque()
+			for _, v := range isConnected[node] {
+				if visited[v] {
+					continue
+				}
+
+				visited[v] = true
+				if v == endIdx {
+					return level
+				}
+				q.Enque(v)
+			} // end loop
+		} // end loop
+	} // end loop
+
+	return -1
+}
+
+func buildConnected(bank []string) [][]int {
+	n := len(bank)
+	isConnected := make([][]int, n)
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if !isMutation(bank[i], bank[j]) {
+				continue
+			}
+			isConnected[i] = append(isConnected[i], j)
+			isConnected[j] = append(isConnected[j], i)
+		}
+	}
+	return isConnected
+}
+
+func pushToBank(bank *[]string, s string) (int, bool) {
+	for i, v := range *bank {
+		if v == s {
+			return i, false
+		}
+	}
+
+	*bank = append(*bank, s)
+	return len(*bank) - 1, true
+}
+
+func isMutation(s1, s2 string) bool {
+	count := 0
+	for i := 0; i < len(s1); i++ {
+		if s1[i] == s2[i] {
+			continue
+		}
+
+		count++
+		if count >= 2 {
+			return false
+		}
+	}
+
+	return count == 1
+}
+
+type Queue struct {
+	a []int
+}
+
+func (q *Queue) Enque(node int) {
+	q.a = append(q.a, node)
+}
+
+func (q *Queue) Deque() int {
+	node := q.a[0]
+	q.a = q.a[1:]
+	return node
+}
+
+func (q *Queue) Size() int {
+	return len(q.a)
+}
