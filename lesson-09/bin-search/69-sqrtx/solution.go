5740533647b378ebd19ab68cbcec35d34e5f2295
nddbao <hikaruit12@gmail.com>
2021-11-01 15:36:58 +0700

    [lesson-09/bin-search] add explain for 69

--- lesson-09/bin-search/69-sqrtx/solution.go
+++ lesson-09/bin-search/69-sqrtx/solution.go
@@ -4,6 +4,20 @@ package solution
 	leetcode: https://leetcode.com/problems/sqrtx/
 */
 
+/*
+	We can check number from 1 to x to find out result. But running time will be: O(n).
+	By using binary search, we can reduce running time to O(log(n))
+	Search from range [low .. high] with init value (low = 1, high = x/2)
+	We have mid = (low + high)/2 and calculate square of mid: exp = mid * mid
+	 + if exp == x: we just return mid
+	 + if exp > x: update high = mid -1
+     + otherwise: update result = mid and low = mid + 1
+	Keep continue when low still less or equal to high.
+
+	Time complexity: O(log(n))
+	Space complexity: O(1)
+*/
+
 func mySqrt(x int) int {
 	if x <= 1 {
 		return x
