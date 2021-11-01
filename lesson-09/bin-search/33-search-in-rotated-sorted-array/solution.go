9ed902fa0222b69425c42e22eaacec38793451e1
nddbao <hikaruit12@gmail.com>
2021-11-01 17:24:19 +0700

    [lesson-09/bin-search] add explain for 33

--- lesson-09/bin-search/33-search-in-rotated-sorted-array/solution.go
+++ lesson-09/bin-search/33-search-in-rotated-sorted-array/solution.go
@@ -4,6 +4,26 @@ package solution
 	leecode: https://leetcode.com/problems/search-in-rotated-sorted-array/
 */
 
+/*
+	We can go through all of elements in array and search for target. But time complexity: O(n).
+
+	Remembering that this is an rotated sorted array.
+	So we can apply binary search with some modifications for updating low and high.
+	Dividing array into two range: A= [low..mid] and B = [mid+1..high]
+
+	We are sure that A or B have to be in order:
+		+ If A is in order and target belongs to that range,
+			update low and high to search target in A. Otherwise, we search B.
+		+ Similarly, if B is in order and target belongs to that range
+			update low and high to search target in B. Otherwise, we search A.
+
+	Our final range will have only one element.
+	Just check this element to know target in nums or not.
+
+	Time complexity: O(log(n))
+	Space complexity: O(1)
+
+*/
 func search(nums []int, target int) int {
 	l, h := 0, len(nums)-1
 	for l < h {
