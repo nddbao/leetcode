528029b5e2f1f56fbed5bf2e34d52702586ca154
nddbao <hikaruit12@gmail.com>
2021-11-04 23:56:05 +0700

    [lesson-09/bst] refactor for 701

---
+++ lesson-09/bst/701-insert-into-a-binary-search-tree/solution.go
@@ -0,0 +1,25 @@
+package solution
+
+/*
+	leetcode: https://leetcode.com/problems/insert-into-a-binary-search-tree/
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	if root.Val < val {
+		root.Right = insertIntoBST(root.Right, val)
+	} else {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+
+	return root
+}
