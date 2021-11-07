63d15e29fdca9eec0f1afa5d94775c0415e6f5f6
nddbao <hikaruit12@gmail.com>
2021-11-07 15:09:36 +0700

    [lesson-09/bst] add explain for 285

--- lesson-09/bst/285-inorder-successor-in-bst/solution.go
+++ lesson-09/bst/285-inorder-successor-in-bst/solution.go
@@ -4,6 +4,18 @@ package solution
 	leetcode: https://leetcode.com/problems/inorder-successor-in-bst/
 */
 
+/*
+	We just search from root to find. Comparing current node value to p value
+	There are there 3  cases:
+		+ node.Val <= p.Val: we know that we need to find bigger value by moving to right side.
+		+ node.Val > p.Val : we know that current node can be our result.
+							Maybe there is a smaller value on the left side that still is greater than p.Val
+							Let try to move to left side to check it.
+
+	Time complexity: O(N)
+	Space Complexity: O(1)
+*/
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
