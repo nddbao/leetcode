14c7045bcbbc7094acaa60091e117662e7fdc112
nddbao <hikaruit12@gmail.com>
2021-11-07 18:14:00 +0700

    [lesson-11/two-pointers] add explain for 26

--- lesson-11/two-pointers/26-remove-duplicates-from-sorted-array/solution.go
+++ lesson-11/two-pointers/26-remove-duplicates-from-sorted-array/solution.go
@@ -4,6 +4,22 @@ package solution
 	leetcode: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 */
 
+/*
+	 We will have two parts in our array:
+		+ First: contains distinct number
+		+ Second: contains elements that we have not visited yet.
+
+	Iterating all of elements in array and using two pointers:
+ 		+ one pointer to keep track (lastIdx + 1) of our first part
+		+ one pointer to keep track index of element we visit.
+    Array is an non-decreasing, so if last item in first part is less than current visited element,
+    We append current element by swapping value based on two pointers.
+
+	Time Complexity: O(n)
+	Space Complexity: O(1)
+
+*/
+
 func removeDuplicates(nums []int) int {
 	if len(nums) == 0 {
 		return 0
