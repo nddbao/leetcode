c5096968fd0f03faf6d813f8a57eefd00d216326
nddbao <hikaruit12@gmail.com>
2021-11-07 20:15:13 +0700

    [lesson-11/two-pointers] add explain for 167

--- lesson-11/two-pointers/167-two-sum-ii-input-array-is-sorted/solution.go
+++ lesson-11/two-pointers/167-two-sum-ii-input-array-is-sorted/solution.go
@@ -4,6 +4,20 @@ package solution
 	leetcode: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 */
 
+/*
+	Using two pointers left and right:
+		+ Left: start from beginning
+		+ Right: start from the end
+	Loop through the array and compute sum = numbers[left] + numbers[right]
+	 + if sum < target, we need increase our sum by moving left one more ahead.
+	 + if sum > target, we need decrease our sum by moving right one more back.
+	 + if sum = target, it means we find out the answer. Just return [left + 1, right + 1] (1-indexed)
+
+	Time Complexity: O(n)
+	Space Complexity: O(1)
+
+*/
+
 func twoSum(numbers []int, target int) []int {
 	l, r := 0, len(numbers)-1
 	for l < r {
