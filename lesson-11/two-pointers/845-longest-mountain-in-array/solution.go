4e24164d058cf32d8a0b5a8fc44b345869f63101
nddbao <hikaruit12@gmail.com>
2021-11-07 17:45:23 +0700

    [lesson-11/two-pointers] add explain for 845

--- lesson-11/two-pointers/845-longest-mountain-in-array/solution.go
+++ lesson-11/two-pointers/845-longest-mountain-in-array/solution.go
@@ -4,6 +4,26 @@ package solution
 	leetcode: https://leetcode.com/problems/longest-mountain-in-array/
 */
 
+/*
+	For a mountain, we need find leftRoot and rightRoot to calculate mountain's length.
+	LeftRoot:
+ 		+ less or equal to previous element
+		+ and less than next element.
+	RightRoot:
+		+ less or equal to next element
+		+ and less than previous element.
+
+	We go through each element to check it.
+	+ rightRoot => check we have leftRoot or not. If yes, then compute length and update max.
+	+ leftRoot => just record index of leftRoot
+	+ check valid mountain or not. If no, reset index of leftRoot.
+		For example: [0 1 2 2 1] -> not valid mountain
+
+	Time Complexity: O(n)
+	Space Complexity: O(1)
+
+*/
+
 func longestMountain(arr []int) int {
 	if len(arr) < 3 {
 		return 0
@@ -16,13 +36,13 @@ func longestMountain(arr []int) int {
 			leftIdx = -1
 		}
 
-		if i+1 < len(arr)-1 && arr[i] == arr[i+1] {
-			leftIdx = -1
-			continue
-		}
-
 		if isLeftRoot(arr, i) {
 			leftIdx = i
+		}
+
+		// check still valid mountain or not
+		if i+1 < len(arr)-1 && arr[i] == arr[i+1] {
+			leftIdx = -1
 		}
 	} // end loop
 
