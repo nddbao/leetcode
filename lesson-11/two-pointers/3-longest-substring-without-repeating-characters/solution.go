43f356f7c791c824b54baeeb40d6102c5db8c986
nddbao <hikaruit12@gmail.com>
2021-11-08 20:37:45 +0700

    [lesson-11/two-pointers] add explain for 3

--- lesson-11/two-pointers/3-longest-substring-without-repeating-characters/solution.go
+++ lesson-11/two-pointers/3-longest-substring-without-repeating-characters/solution.go
@@ -4,6 +4,17 @@ package solution
 	leetcode: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 */
 
+/*
+	Using two pointers to maintain a sliding window that contain unique characters.
+	We also use extra map to keep track character in window.
+	Iterate each character in string, use this map to check current character already in window or not.
+		+ If not, we delete s[left] character and move left ahead one step until current character not in map anymore.
+		+ Otherwise, just add current character to map and update max value.
+
+	Time complexity: O(n)
+	Space complexity: O(n)
+*/
+
 func lengthOfLongestSubstring(s string) int {
 	distinct := make(map[byte]struct{})
 
