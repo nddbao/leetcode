4fb6f8da6ceee7aab61140e0b5855e373714bfd6
nddbao <hikaruit12@gmail.com>
2021-11-11 13:44:06 +0700

    [lesson-11/two-pointers] add explain for 1099

--- lesson-11/two-pointers/1099-two-sum-less-than-k/solution.go
+++ lesson-11/two-pointers/1099-two-sum-less-than-k/solution.go
@@ -6,6 +6,21 @@ import "sort"
 	leetcode: https://leetcode.com/problems/two-sum-less-than-k/
 */
 
+/*
+	First we sort array and apply two pointers (left, right)
+     + Left start from beginning:  move to right side.
+	 + Right start from the end: move to left side.
+
+	We have max to keep track our largest sum < k.
+	For each time, we check sum = nums[left] + nums[right]
+	We divide it into two cases:
+		+ sum < k: just update max and move left
+		+ sum >= k: move right
+
+
+	Time complexity: O(nlogn) + O(n) -> O(nlogn)
+	Space complexity: O(n) or O(1) depend on sort
+*/
 func twoSumLessThanK(nums []int, k int) int {
 	n := len(nums)
 	if n < 2 {
