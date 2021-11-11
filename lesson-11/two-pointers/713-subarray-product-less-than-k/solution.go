0cd49a501f54066fe879f30fdc5c6b7a7b29c17a
nddbao <hikaruit12@gmail.com>
2021-11-11 22:36:46 +0700

    [lesson-11/two-pointers] add explain for 713

--- lesson-11/two-pointers/713-subarray-product-less-than-k/solution.go
+++ lesson-11/two-pointers/713-subarray-product-less-than-k/solution.go
@@ -4,6 +4,20 @@ package solution
 	leetcode: https://leetcode.com/problems/subarray-product-less-than-k/
 */
 
+/*
+	We use two pointers [left .. right] to keep track range.
+	That range will has  product but less than K.
+	Left and right pointer will move to right side
+
+	First, we set left and right at the begin.
+	Then we  have a loop to move right by one:
+		+ Calculate new product.
+		+ If prod >=k, we have to move left by one until we have prod < k.
+		+ Then, we will add len(range) to our result.
+
+	Time complexity: O(n)
+	Space complexity: O(1)
+*/
 func numSubarrayProductLessThanK(nums []int, k int) int {
 	if k <= 1 {
 		return 0
