11c4c61b25c3de27bcb88a36de28b1e94922b9da
nddbao <hikaruit12@gmail.com>
2021-11-11 19:24:01 +0700

    [lesson-11/two-pointers] add explain for 340

--- lesson-11/two-pointers/340-longest-substring-with-at-most-k-distinct-characters/solution.go
+++ lesson-11/two-pointers/340-longest-substring-with-at-most-k-distinct-characters/solution.go
@@ -4,6 +4,21 @@ package solution
 	leetcode: https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
 */
 
+/*
+	We define a Frequencies to keep occurrences of character using []int with 256 length.
+	We use two pointers to keep track range [left ... right] that have number of unique characters <= k.
+    Every time, moving right pointer ahead by one. We add one to its occurrence in Freq.
+	Then we check numberOfUniques in Freq:
+		+ if numberOfUniques <= k: do nothing
+		+ otherwise: we will move left by one until numberOfUniques <= k.
+					We also need to update its occurrence in Freq.
+    We have max variable to keep track our longest length.
+	Each time we move for right pointer, will update it.
+
+	Time complexity: O(n)
+	Space complexity: O(256) -> O(1)
+*/
+
 func lengthOfLongestSubstringKDistinct(s string, k int) int {
 	if k == 0 {
 		return 0
