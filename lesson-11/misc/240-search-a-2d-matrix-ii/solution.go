74581b726dcb99af8a542294df6aa192b012405c
nddbao <hikaruit12@gmail.com>
2021-11-12 16:28:19 +0700

    [lesson-11/misc] add explain for 240

--- lesson-11/misc/240-search-a-2d-matrix-ii/solution.go
+++ lesson-11/misc/240-search-a-2d-matrix-ii/solution.go
@@ -4,11 +4,28 @@ package solution
 	leetcode: https://leetcode.com/problems/search-a-2d-matrix-ii/
 */
 
+/*
+	Row in ascending from left -> right
+	Column in ascending from top -> bottom
+
+    Let's  pick cell that starts at bottom-left (i=m-1, j=0)
+ 	There are 3 cases:
+		+ current value == target: we found it.
+		+ current value < target:  check right cell of current cell.
+		+ current value > target: check up cell of current cell
+
+	We continue until we out of index and we found value == target.
+
+
+	Time complexity: O(m + n)
+	Space complexity: O(1)
+*/
+
 func searchMatrix(matrix [][]int, target int) bool {
 	m := len(matrix)
 	n := len(matrix[0])
 
-	i, j := m-1, 0
+	i, j := m-1, 0 // bottom-left
 	for isValidIdx(m, n, i, j) {
 		val := matrix[i][j]
 		if val == target {
