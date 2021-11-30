d7369de692b89194161775d34e6344d44b99ff0e
nddbao <hikaruit12@gmail.com>
2021-11-30 16:59:53 +0700

    [lesson-13/union-find] add solution for 990

---
+++ lesson-13/union-find/990-satisfiability-of-equality-equations/solution.go
@@ -0,0 +1,95 @@
+package solution
+
+/*
+	leetcode: https://leetcode.com/problems/satisfiability-of-equality-equations/
+*/
+
+func equationsPossible(equations []string) bool {
+	ds := NewDisjointSet()
+
+	// merge all variables that have same value
+	for _, s := range equations {
+		eq := Equation(s)
+		if !eq.IsEqual() {
+			continue
+		}
+
+		first, second := eq.FirstVal(), eq.SecondVal()
+		ds.Union(first, second)
+
+	} // end loop
+
+	// check variables that have different value
+	for _, s := range equations {
+		eq := Equation(s)
+		if eq.IsEqual() {
+			continue
+		}
+
+		first, second := eq.FirstVal(), eq.SecondVal()
+		u, v := ds.Find(first), ds.Find(second)
+		if u == v {
+			return false
+		}
+	} // end loop
+
+	return true
+}
+
+type Equation string
+
+func (e Equation) FirstVal() byte {
+	return e[0] - 'a'
+}
+
+func (e Equation) SecondVal() byte {
+	return e[3] - 'a'
+}
+
+func (e Equation) IsEqual() bool {
+	return e[1] == '='
+}
+
+func NewDisjointSet() *DisjointSet {
+	n := byte('z' - 'a' + 1)
+	parent := make([]byte, n)
+	rank := make([]int, n)
+	for i := 0; i < len(parent); i++ {
+		parent[i] = byte(i)
+	}
+
+	return &DisjointSet{parent: parent, rank: rank}
+}
+
+type DisjointSet struct {
+	parent []byte
+	rank   []int
+}
+
+func (d *DisjointSet) Find(v byte) byte {
+	if v == d.parent[v] {
+		return v
+	}
+
+	d.parent[v] = d.Find(d.parent[v])
+	return d.parent[v]
+}
+
+func (d *DisjointSet) Union(u, v byte) {
+	u = d.Find(u)
+	v = d.Find(v)
+	if u == v {
+		return
+	}
+
+	// update parent and rank
+	if d.rank[u] < d.rank[v] {
+		u, v = v, u
+	}
+	d.parent[v] = u
+
+	// update rank
+	if d.rank[u] == d.rank[v] {
+		d.rank[u]++
+	}
+}
