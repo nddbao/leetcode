0582453eb02885dc8a2276fa36dda38a0436b523
nddbao <hikaruit12@gmail.com>
2021-12-28 21:45:28 +0700

    [lesson-17/dp] add explain for 63

---
+++ lesson-17/dp/63-unique-paths-ii/solution.go
@@ -0,0 +1,38 @@
+package solution
+
+/*
+	leetcode: https://leetcode.com/problems/unique-paths-ii/
+*/
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+	if obstacleGrid[m-1][n-1] == 1 {
+		return 0
+	}
+
+	dp := NewDP(m+1, n+1)
+	for i := m - 1; i >= 0; i-- {
+		for j := n - 1; j >= 0; j-- {
+			if i == m-1 && j == n-1 {
+				dp[i][j] = 1
+				continue
+			}
+
+			if obstacleGrid[i][j] == 1 {
+				dp[i][j] = 0
+			} else {
+				dp[i][j] = dp[i][j+1] + dp[i+1][j]
+			}
+		} // end loop
+	} // end loop
+
+	return dp[0][0]
+}
+
+func NewDP(m, n int) [][]int {
+	a := make([][]int, m)
+	for i := 0; i < len(a); i++ {
+		a[i] = make([]int, n)
+	}
+	return a
+}
