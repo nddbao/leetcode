50f8b9c74c4633aa02afeee875bc05e1acad0e46
nddbao <hikaruit12@gmail.com>
2021-12-28 09:38:56 +0700

    [lesson-17/dp] add explain for 1143

--- lesson-17/dp/1143-longest-common-subsequence/solution.go
+++ lesson-17/dp/1143-longest-common-subsequence/solution.go
@@ -4,6 +4,18 @@ package solution
 	leetcode: https://leetcode.com/problems/longest-common-subsequence/
 */
 
+/*
+	We check each character in text1 and text2 corresponding at index i and j.
+	There are two cases:
+		+ text[i] equal to text[j]: just count for that and move i and j one step ahead.
+		+ text[i] Not equal to text[j]: now we have get max when finding longest common subsequence in two cases:
+				- i+1 for text1, j for text2
+				- i for text1, j + 1 for text1
+	We also know that there are always repeats steps, so we can use cache to avoid duplication.
+
+ 	Time complexity: O(n*m) where n is len of text1, m is len of text2
+	Space complexity: O(n*m)
+*/
 func longestCommonSubsequence(text1 string, text2 string) int {
 	cache := NewCache(len(text1), len(text2))
 	return helper(cache, text1, 0, text2, 0)
