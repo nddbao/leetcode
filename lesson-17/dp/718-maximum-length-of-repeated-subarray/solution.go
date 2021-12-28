74240c5d0ce26dd462dec056a20f41ccf2c9301f
nddbao <hikaruit12@gmail.com>
2021-12-28 11:19:14 +0700

    [lesson-17/dp] add explain for 718

--- lesson-17/dp/718-maximum-length-of-repeated-subarray/solution.go
+++ lesson-17/dp/718-maximum-length-of-repeated-subarray/solution.go
@@ -4,6 +4,18 @@ package solution
 	leetcode: https://leetcode.com/problems/maximum-length-of-repeated-subarray/
 */
 
+/*
+	We check each element of two array corresponding at index i  and j.
+	Comparing between them nums1[i] and nums2[j].
+	There are two cases:
+		+ nums1[i] == nums2[j]: update dp[i][j] = dp[i+1][j+1] + 1 and max
+		+ otherwise: do nothing.
+
+
+	Time complexity: O(m*n) where m is len of nums1, n is len of nums2
+	Space complexity: O(m*n)
+*/
+
 func findLength(nums1 []int, nums2 []int) int {
 	m := len(nums1)
 	n := len(nums2)
